a483e66f9854a14d9baa965aa4c18e64723b2674
Jeff McCune <jeff@puppetlabs.com>
2010-08-05 21:26:47 -0700

    Added Sun JDK class
    Signed-off-by: Jeff McCune <jeff@puppetlabs.com>

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: puppetlabs-tomcat
+#
+#   This class models the tomcat service in Puppet
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class puppetlabs-tomcat {
+  $module = "puppetlabs-tomcat"
+  notify { "FIXME: ${module} unimplemented": }
+  # statements
+}
---
+++ manifests/sunjdk.pp
@@ -0,0 +1,39 @@
+# Class: puppetlabs-tomcat::sunjdk
+#
+#   This class models the Sun Java Development Kit in Puppet
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class puppetlabs-tomcat::sunjdk inherits puppetlabs-tomcat {
+  $module    = "puppetlabs-tomcat"
+  $class     = "${module}::sunjdk"
+  $prefix    = "/etc/puppet/modules"
+  $p1        = "${prefix}/${module}"
+  $p2        = "puppet:///modules/${module}"
+  $installer = "jdk-6u21-linux-${architecture_real}-rpm.bin"
+
+  $architecture_real = $architecture ? {
+    "x86_64" => "x64",
+    default  => $architecture,
+  }
+
+  Exec { path => "/usr/kerberos/sbin:/usr/kerberos/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin" }
+
+  file {
+    "/var/tmp/${installer}":
+      source  => [ "${p1}/${installer}", "${p2}/${installer}" ],
+      recurse => "false"
+  }
+  exec { "install-jdk":
+    command     => [ "/var/tmp/${installer}", "-noregister" ],
+    refreshonly => true,
+    subscribe   => File["/var/tmp/${installer}"],
+  }
+  # statements
+}

9bbf73c2dbdd450e6be85a18fe0ccaea10ef9c12
Jeff McCune <jeff@puppetlabs.com>
2010-08-05 21:57:43 -0700

    Added unpacking of Tomcat 5.5.30 install media.
    Signed-off-by: Jeff McCune <jeff@puppetlabs.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,6 +12,27 @@
 #
 class puppetlabs-tomcat {
   $module = "puppetlabs-tomcat"
-  notify { "FIXME: ${module} unimplemented": }
-  # statements
+  $prefix = "/etc/puppet/modules"
+# JJM Look for files on the node filesystem first.
+  $p1 = "${prefix}/${module}"
+# JJM Look for files on the puppetmaster second.
+  $p2 = "puppet:///modules/${module}"
+# Our installer media.
+  $tomcat_version = "5.5.30"
+  $installer = "apache-tomcat-${tomcat_version}.tar.gz"
+
+  File { owner => "0", group => "0", mode  => "0644" }
+  Exec { path => "/usr/kerberos/sbin:/usr/kerberos/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin" }
+
+  file {
+    "/var/tmp/${installer}":
+      source  => [ "${p1}/${installer}", "${p2}/${installer}" ],
+  }
+  exec {
+    "unpack-tomcat":
+      command => "tar -C /usr -x -z -f /var/tmp/${installer}",
+      creates => "/usr/apache-tomcat-${tomcat_version}",
+      require => File["/var/tmp/${installer}"],
+  }
+  file { "/usr/tomcat": ensure => "/usr/apache-tomcat-${tomcat_version}" }
 }
